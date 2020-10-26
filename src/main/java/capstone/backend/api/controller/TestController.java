947c79ab6569142fba4d72f45145aae36b2a1944
bluemarble <hoangledinh65@gmail.com>
2020-10-26 12:22:34 +0700

    today

--- src/main/java/capstone/backend/api/controller/TestController.java
+++ src/main/java/capstone/backend/api/controller/TestController.java
@@ -19,7 +19,7 @@ public class TestController {
         Set<String> roles = new HashSet<>();
         roles.add(RoleUtils.ROLE_EMPLOYEE);
         return UserRegisterDto.builder()
-                .email("hoang7.com")
+                .email("hoang8.com")
                 .password("123445").dob("22/11/1998")
                 .fullName("Le Son Tung")
                 .phoneNumber("0342529999")
