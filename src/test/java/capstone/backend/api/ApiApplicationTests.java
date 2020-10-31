dd7ec4546fd7fc7a99f43f480152e55f95dfa68a
tung beo <sontung199x@gmail.com>
2020-10-31 23:08:00 +0700

    commit code: hide all test demo

--- src/test/java/capstone/backend/api/ApiApplicationTests.java
+++ src/test/java/capstone/backend/api/ApiApplicationTests.java
@@ -21,11 +21,11 @@ class ApiApplicationTests {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    public void test1() throws Exception {
-        this.mockMvc.perform(get("/api/test/teacherRegister")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("hoang5.com")));
-    }
+//    @Test
+//    public void test1() throws Exception {
+//        this.mockMvc.perform(get("/api/test/teacherRegister")).andDo(print()).andExpect(status().isOk())
+//                .andExpect(content().string(containsString("hoang5.com")));
+//    }
 
     // @Test
     // public void test2() throws Exception {
@@ -48,21 +48,21 @@ class ApiApplicationTests {
     // 			.andExpect(jsonPath("$.gender").value("female"));
     // }
 
-    @Test
-    public void test4() throws Exception {
-        // InputStream is = new FileInputStream("./jsonschema.json");
-        // BufferedReader buf = new BufferedReader(new InputStreamReader(is));
-        // String line = buf.readLine();
-        // StringBuilder sb = new StringBuilder();
-        // while (line != null) {
-        // 	sb.append(line).append("\n");
-        // 	line = buf.readLine();
-        // }
-        // String fileAsString = sb.toString();
-
-        // JSONTokener schemaData = new JSONTokener(fileAsString);
-        // // JSONObject jsonschema = new JSONObject(schemaData);
-        // System.out.println(fileAsString);
-    }
+//    @Test
+//    public void test4() throws Exception {
+//        // InputStream is = new FileInputStream("./jsonschema.json");
+//        // BufferedReader buf = new BufferedReader(new InputStreamReader(is));
+//        // String line = buf.readLine();
+//        // StringBuilder sb = new StringBuilder();
+//        // while (line != null) {
+//        // 	sb.append(line).append("\n");
+//        // 	line = buf.readLine();
+//        // }
+//        // String fileAsString = sb.toString();
+//
+//        // JSONTokener schemaData = new JSONTokener(fileAsString);
+//        // // JSONObject jsonschema = new JSONObject(schemaData);
+//        // System.out.println(fileAsString);
+//    }
 
 }
