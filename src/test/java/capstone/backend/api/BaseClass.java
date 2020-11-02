142fe9ad592da5576b520ae1086777e4a58ab66b
bluemarble <hoangledinh65@gmail.com>
2020-11-02 11:44:27 +0700

    viet demo testcase cho objective by hoang

---
+++ src/test/java/capstone/backend/api/BaseClass.java
@@ -0,0 +1,29 @@
+package capstone.backend.api;
+
+import  capstone.backend.api.configuration.CommonProperties;
+import capstone.backend.api.utils.RestUtils;
+import static com.jayway.restassured.RestAssured.*;
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.Header;
+
+import org.junit.Before;
+
+import static org.hamcrest.Matchers.*;
+public class BaseClass {
+
+    String token = "";
+    @Before
+    public void setup() {
+		// Test Setup
+		RestUtils.setBaseURI("http://bluemarble97.com"); // Setup Base URI
+		RestUtils.setPort(8081);
+		RestUtils.setBasePath("api"); // Setup Base Path
+		RestUtils.setContentType(ContentType.JSON); // Setup Content Type
+		String requestBody = "{\"email\" : \"sontung199x@gmail.com\" , \"password\" : \"123445\"} ";
+		
+		token = given().contentType(ContentType.JSON)
+		.body(requestBody).when().post("auth/signin/")
+		.jsonPath().get("data.jwtToken");
+	}
+
+}
---
+++ src/test/java/capstone/backend/api/ObjectivesTest.java
@@ -0,0 +1,111 @@
+package capstone.backend.api;
+import  capstone.backend.api.configuration.CommonProperties;
+import capstone.backend.api.utils.RestUtils;
+import org.testng.annotations.BeforeSuite;
+import org.junit.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static com.jayway.restassured.RestAssured.*;
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.Header;
+
+import static org.hamcrest.Matchers.*;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ObjectivesTest extends  BaseClass{
+
+	@Test
+	public void T01() {
+		Header headers = new Header("Authorization", "lidi " +token);
+		given().header(headers).when()
+		.get("objective/key_result/23/")
+		.then()
+		.assertThat()
+		.body("message", equalTo("Thành công"))
+		.body("code", equalTo(200))
+		.body(matchesJsonSchemaInClasspath("jsonschema.json"));
+	}
+
+	@Test
+	public void T02() {
+		Header headers = new Header("Authorization", "lidi " +token);
+		given().header(headers).when()
+		.get("objective/key_result/22/")
+		.then()
+		.assertThat()
+		.body("message", equalTo("Thành công"))
+		.body("code", equalTo(200))
+		.body(matchesJsonSchemaInClasspath("jsonschema.json"));
+	}
+	@Test
+	public void T03() {
+		Header headers = new Header("Authorization", "lidi " +token);
+		given().header(headers).when()
+		.get("objective/key_result/1")
+		.then()
+		.assertThat()
+		.body("message", equalTo("Thành công"))
+		.body("code", equalTo(200));
+	}
+	@Test
+	public void T04() {
+		Header headers = new Header("Authorization", "lidi " +token);
+		given().header(headers).when()
+		.get("objective/key_result/-1")
+		.then()
+		.assertThat()
+		.body("message", equalTo("Không tìm thấy dữ liệu"))
+		.body("code", equalTo(404))
+		.body(matchesJsonSchemaInClasspath("404ErrorSchema.json"));
+	}
+	@Test
+	public void T05() {
+		Header headers = new Header("Authorization", "lidi " +token);
+		given().header(headers).when()
+		.get("objective/key_result/0")
+		.then()
+		.assertThat()
+		.body("message", equalTo("Không tìm thấy dữ liệu"))
+		.body("code", equalTo(404))
+		.body(matchesJsonSchemaInClasspath("404ErrorSchema.json"));
+	}
+	@Test
+	public void T06() {
+		Header headers = new Header("Authorization", "lidi " +token);
+		given().header(headers).when()
+		.get("objective/key_result/sdf")
+		.then()
+		.assertThat()
+		.body("status", equalTo(400))
+		.body("error", equalTo("Bad Request"))
+		.body(matchesJsonSchemaInClasspath("400ErrorSchema.json"));
+
+	}
+	@Test
+	public void T07() {
+		Header headers = new Header("Authorization", "lidi " +token);
+		given().header(headers).when()
+		.get("objective/key_result/.+342as")
+		.then()
+		.assertThat()
+		.body("status", equalTo(400))
+		.body("error", equalTo("Bad Request"))
+		.body(matchesJsonSchemaInClasspath("400ErrorSchema.json"));
+	}
+	@Test
+	public void T08() {
+		Header headers = new Header("Authorization", "lidi " +token);
+		given().header(headers).when()
+		.get("objective/key_result/....")
+		.then()
+		.assertThat()
+		.body("status", equalTo(400))
+		.body("error", equalTo("Bad Request"))
+		.body(matchesJsonSchemaInClasspath("400ErrorSchema.json"));
+
+	}
+}
